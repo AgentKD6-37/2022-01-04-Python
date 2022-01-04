3f85dda1971cf81b83c28bdc671ffe0f2a067d3e
Michael  Stack <mstack000@gmail.com>
2022-01-04 13:37:48 -0500

    Lab 8 added

---
+++ Day 1/deleteme.py
@@ -0,0 +1,5 @@
+#!/bin/rm
+# This script calls on the executable called **rm** (remove), which is
+# installed to the */bin* directory. When this script is run,
+# bash will execute this script with *rm*, which will remove (delete)
+# the script :p
