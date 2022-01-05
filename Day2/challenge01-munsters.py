04ca3f242eba68c7b70803ce7a5a706fcf0a1fe7
Michael  Stack <mstack000@gmail.com>
2022-01-05 10:47:14 -0500

    munsters challenge01-munsters.py complete

---
+++ Day2/challenge01-munsters.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser
+   Review of Lists and Dictionaries"""
+
+# define a short data set (in real world, we want to read this from a file or API)
+munsters = {'endDate': 1966, 'startDate': 1964,\
+        'names':['Lily', 'Herman', 'Grandpa', 'Eddie', 'Marilyn']}   # {} creates dict
+
+# Your solution goes below this line
+# ----------------------------------
+print(munsters.get('names'))
+print(munsters.get('endDate'))
+print(munsters.get('startDate'))
+
+munsters['episodes'] = 70
