021a853a6ad458f72a3ae8ce82d163bee573c98f
Michael  Stack <mstack000@gmail.com>
2022-01-05 16:09:16 -0500

    fixed challenge02-ifelse.py again

--- Day2/challenge02-ifelse.py
+++ Day2/challenge02-ifelse.py
@@ -3,19 +3,20 @@
 def main():
     print("Welcome to the network grader!")
     user_input = input("How fast is your wi-fi network? \n(answers must be a number from 1MBPS to 1000MPS for gigabyte and above speeds)\n")
-    int_user_input = int(user_input)
-    if int_user_input >= 1000:
-        print("Your Wi-fi is grade S! Fantastic!")
-    elif int_user_input >=500:
-        print("Your Wi-fi is grade A. Pretty Fast, but not the best.")
-    elif int_user_input >= 200:
-        print("Your Wi-fi is grade B. Quick but definitely could be better.")
-    elif int_user_input >= 100:
-        print("Your Wi-fi is grade C. You might need an upgrade.")
-    elif int_user_input >= 50:
-        print("Your Wi-fi is grade D. Do you live in the country?")
-    elif int_user_input < 50:
-        print("Your Wi-fi is grade F! Oh no!")
+    if user_input == int:
+        int_user_input = int(user_input)
+        if int_user_input >= 1000:
+            print("Your Wi-fi is grade S! Fantastic!")
+        elif int_user_input >= 500:
+            print("Your Wi-fi is grade A. Pretty Fast, but not the best.")
+        elif int_user_input >= 200:
+            print("Your Wi-fi is grade B. Quick but definitely could be better.")
+        elif int_user_input >= 100:
+            print("Your Wi-fi is grade C. You might need an upgrade.")
+        elif int_user_input >= 50:
+            print("Your Wi-fi is grade D. Do you live in the country?")
+        elif int_user_input < 50:
+            print("Your Wi-fi is grade F! Oh no!")
     else:
         print("Invalid input! Try again!")
 
