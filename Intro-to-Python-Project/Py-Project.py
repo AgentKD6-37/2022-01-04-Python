0d791d74ad4a7200bf7a0b35973b725f1e3c372e
Michael  Stack <mstack000@gmail.com>
2022-01-10 00:34:32 -0500

    API call working as intended. Pokedex functionality completed.

--- Intro-to-Python-Project/Py-Project.py
+++ Intro-to-Python-Project/Py-Project.py
@@ -3,23 +3,53 @@
    Project written by Michael Stack github: https://github.com/AgentKD6-37
    This program is the PokeDex! It takes a user input on what Pokemon they see and returns the Pokemon's info from
    https://pokeapi.co for usage see the repo license"""
+
+import json
+from random import randint
+
 import requests
 
-API = f"https://pokeapi.co/api/v2/pokemon/"
+API_Pokemon = f"https://pokeapi.co/api/v2/pokemon/"
+API_Species = f"https://pokeapi.co/api/v2/pokemon-species/"
+
+
+def start_pokedex():
+    print("Pokedex v1.0 is starting up!")
+    user_input = input("What Pokemon would you like to identify? \n").lower()
+    return user_input
+
 
 def get_pokemon(pokemon):
     if pokemon:
-        url = API + pokemon + "/"
-        data = requests.get(url)
-    return data
+        poke_url = API_Pokemon + pokemon + "/"
+        poke_data = requests.get(poke_url).json()
+        poke_id = str(poke_data["id"])
+        species_data = requests.get(API_Species + poke_id + "/").json()
+        species_data.update(poke_data)
+    else:
+        start_pokedex()
+    return species_data
 
-def pretty_response(pokemon):
-    return
+
+def pretty_response(data):
+    random_flavor_text = randint(0, 15)
+    if data:
+        pokemon_name = data["name"].capitalize()
+        pokemon_type = data["types"][0]["type"]["name"]
+        pokemon_height = data["height"]
+        pokemon_weight = (data["weight"]) / 10
+        pokemon_color = data["color"]["name"]
+        pokemon_flavor_text = data["flavor_text_entries"][random_flavor_text]["flavor_text"]
+        print(
+            f"{pokemon_name} is a {pokemon_type} Pokemon. Commonly {pokemon_height}m tall and weighing on"
+            f" average {pokemon_weight}kg. This pokemon is {pokemon_color} in color. {pokemon_flavor_text}"
+        )
+
 
 def main():
-    print("Pokedex v1.0 is starting up!")
-    user_input = input("What Pokemon would you like to identify").lower()
+    user_input = start_pokedex()
     data = get_pokemon(user_input)
-    print(data.json())
+    pretty_response(data)
+
 
 main()
