b6dc369862d7e7dfb20c9105772cade34498f5ec
Michael  Stack <mstack000@gmail.com>
2022-01-13 10:36:10 -0500

    labs and other changes

---
+++ Day 7/flaskapi/jinja2temp01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/hosts")
+@app.route("/")
+def index():
+    return render_template("hellobasic.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=2224)
